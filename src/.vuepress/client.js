359d8b32764045d622db6411b8d2bdc607b1151e
fhkk55 <fhkk55@noreply.gitcode.com>
2025-07-20 10:56:13 +0800

    修改样式

--- src/.vuepress/client.js
+++ src/.vuepress/client.js
@@ -1,6 +1,12 @@
 import { defineClientConfig } from "vuepress/client";
 import Blog from "./layouts/Blog.vue";
 
+//为项目主页的特性添加闪光效果。
+import "vuepress-theme-hope/presets/shinning-feature-panel.scss";
+//将博主头像裁剪为圆形
+import "vuepress-theme-hope/presets/round-blogger-avatar.scss";
+//为页面图标添加鼠标悬停的跳动效果。
+import "vuepress-theme-hope/presets/bounce-icon.scss";
 export default defineClientConfig({
   //...
 
